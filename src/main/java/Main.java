f5f9bb7a3fd4c75313851d00807b84e0b837b12d
Guillermo <guillermo.guerrero@simpple.cat>
2014-03-24 12:54:55 +0100

    New library and save properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import com.sun.deploy.util.StringUtils;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
 import java.util.List;
 import java.util.Properties;
 
@@ -18,28 +19,53 @@ public class Main {
     private PtumblrForm form_ui;
 
 
-
     //Load config.properties
-    private void loadProperties(String fileName) throws IOException{
+    private void loadProperties(String fileName) throws IOException {
         Properties properties = new Properties();
-        InputStream input = Main.class.getResourceAsStream(fileName);
-        properties.load(input);
+
+        try {
+            InputStream input = Main.class.getResourceAsStream(fileName);
+            properties.load(input);
+        } catch (IOException e) {
+            System.out.println("Can't load properties in " + fileName);
+        }
 
         input_folder = properties.getProperty("input_folder", "input");
         output_folder = properties.getProperty("output_folder", "output");
         String strTags = properties.getProperty("default_tags", "");
-        tags =  strTags.split(",");
+        tags = strTags.split(",");
+    }
+
+    private void saveProperties(String fileName) {
+        try {
+            OutputStream output = new FileOutputStream(fileName);
+
+            Properties properties = new Properties();
+            properties.setProperty("input_folder", input_folder);
+            properties.setProperty("output_folder", output_folder);
+            properties.setProperty("default_tags", org.apache.commons.lang.StringUtils.join(tags, ","));
+
+            properties.store(output, "");
+        } catch (IOException e) {
+            System.out.println("Can't save properties in " + fileName);
+        }
     }
 
 
     //Initialize user interface
-    public void initializeUI(){
+    public void initializeUI() {
         form_ui = new PtumblrForm(input_folder, output_folder, tags);
+        form_ui.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                saveProperties("/config.properties");
+            }
+        });
     }
 
 
     //Start application
-    public void start(String args[]) throws IOException{
+    public void start(String args[]) throws IOException {
         loadProperties("/config.properties");
         initializeUI();
     }
--- src/main/java/PtumblrForm.java
+++ src/main/java/PtumblrForm.java
@@ -2,6 +2,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
